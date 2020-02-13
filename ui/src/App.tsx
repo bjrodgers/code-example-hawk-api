2a51a5b90529a1ccef236b9ef5a3b44278c224fc
Brandon Rodgers <bj.rodgers@gmail.com>
2020-02-12 22:47:49 -0700

    Load hawks and display them in a table

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import HawkTable from './components/HawkTable';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <h1>hello world</h1>
+type AppProps = {}
+
+export default class App extends Component<AppProps> {
+  static defaultProps = {}
+
+  render() {
+     return <div className='App v-flex'>
+      <button className='add-hawk-button'>Add Hawk</button>
+      <div className='h-flex'>
+        <input className='filter-text'></input>
+        <button className='filter-button'>Filter</button>
+      </div>
+      <div className='h-flex'>
+        <HawkTable/>
+      </div>
     </div>
-  );
+  }
 }
 
-export default App;
--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,6 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore, Store } from 'redux';
+import thunk from "redux-thunk";
 import App from './App';
+import './index.css';
+import { loadHawksAction } from './redux/hawkActions';
+import hawkReducer from './redux/hawkReducer';
+import IAppState from './models/IAppState';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store:Store<IAppState, any> = createStore(hawkReducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById("root")
+);
+
+
+// let hawk1:IHawk = {
+//     id:2,
+//     name:'Hawk Two', 
+//     gender:'FEMALE',
+//     size:'LARGE',
+//     behaviorDescription: '',
+//     colorDescription: 'Black',
+//     habitatDescription: '',
+//     lengthBegin: 1,
+//     lengthEnd: 3,
+//     pictureUrl: '',
+//     weightBegin: 5,
+//     weightEnd: 7,
+//     wingspanBegin: 10,
+//     wingspanEnd: 12
+// }
+// store.dispatch(saveHawkAction(hawk1));
+
+
+store.dispatch(loadHawksAction());
